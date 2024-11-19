5565e0240bc977aa90a4d73fc0ff1d9c0c1d5915
dwisyafriadi2 <dwisyafriadi15@gmail.com>
2024-11-19 18:44:37 +0700

    Update Repo

--- celestiadocker.sh
+++ celestiadocker.sh
@@ -1,9 +1,32 @@
 #!/bin/bash
 
-# Display the banner from the given URL
-echo -e '\n\e[42mDisplay Banner\e[0m\n' && sleep 1
-sleep 1 && curl -s https://raw.githubusercontent.com/dwisyafriadi2/celestia-installer/refs/heads/main/banner.sh | bash && sleep 1
+# Function to print the banner
+print_banner() {
+  clear
+  echo -e """
+    ____                       
+   / __ \____ __________ ______
+  / / / / __ \`/ ___/ __ \`/ ___/
+ / /_/ / /_/ (__  ) /_/ / /    
+/_____/_\\__,_/____/\\__,_/_/      
 
+    ____                       __
+   / __ \\___  ____ ___  __  __/ /_  ______  ____ _
+  / /_/ / _ \\/ __ \`__ \\/ / / / / / / / __ \\/ __ \`/
+ / ____/  __/ / / / / / /_/ / / /_/ / / / / /_/ / 
+/_/    \\___/_/ /_/ /_/\\__,_/_/\\__,_/_/ /_/\\__, /  
+                                         /____/    
+
+====================================================
+     Automation         : Auto Install Node 
+     Telegram Channel   : @dasarpemulung
+     Telegram Group     : @parapemulung
+====================================================
+"""
+}
+
+# Call the print_banner function
+print_banner
 # Set environment variables
 export NETWORK=celestia
 export NODE_TYPE=light
